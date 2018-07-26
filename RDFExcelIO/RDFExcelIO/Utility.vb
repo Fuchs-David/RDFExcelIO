fd3628ee06e9edaf8d85f5508fca04d14364bdb1
David Fuchs <fucd00@vse.cz>
2018-07-26 10:11:58 +0200

    Upgraded packages and changed code signing key

---
+++ RDFExcelIO/RDFExcelIO/Utility.vb
@@ -0,0 +1,9 @@
+﻿Module Utility
+    Public Function ToSafeString(ByVal objectToString As Object) As String
+        If objectToString Is Nothing Then
+            Return ""
+        Else
+            Return objectToString.ToString
+        End If
+    End Function
+End Module
